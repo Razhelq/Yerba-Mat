2c6d3290d4fd264e131757862628bfa6a7022019
Mateusz Szpakowski <mateuszszpakowski@wp.pl>
2018-09-02 15:34:38 +0200

    model corrected

--- yerbamat/yerba_mat/models.py
+++ yerbamat/yerba_mat/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.CharField(max_length=1024, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     amount = models.IntegerField(default=0)
-    rate = models.FloatField
+    rate = models.FloatField(default=0)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     picture = models.ImageField(
         verbose_name='pictures',
